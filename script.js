36fc934c1fceb464ff85d5aaade82c51b9459692
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2024-10-23 14:12:35 -0400

    create function createPassword()

--- script.js
+++ script.js
@@ -31,6 +31,16 @@ const upperCase = [
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 const symbols = ["!", "@", "#", "$", "%", "&", "*"];
 
+const allChars = upperCase + numbers + symbols;
+
 function createPassword() {
   let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+  password += symbols[Math.floor(Math.random() * symbols.length)];
+
+  while (passwordLength.value > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
 }
