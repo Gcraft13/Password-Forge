e737a35936e5ca264827a03fa29f559bac0ab24b
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2024-10-23 12:55:24 -0400

    Add js for password

---
+++ script.js
@@ -0,0 +1,36 @@
+const passwordBox = $("#password");
+const passwordLength = $("#materials");
+
+const upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const symbols = ["!", "@", "#", "$", "%", "&", "*"];
+
+function createPassword() {
+  let password = "";
+}
